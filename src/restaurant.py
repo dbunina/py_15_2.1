029955b252b1b316c64a7c6e1ad5d5d8e65a5cb3
Diana Bunina <d.bunina@hh.ru>
2018-04-15 18:28:55 +0300

    small refactoring, fix codestyle

--- src/restaurant.py
+++ src/restaurant.py
@@ -4,28 +4,37 @@
 cook_book = dict()
 order_list = dict()
 
+
 def read_order_list():
     with open('order.txt') as f:
         for raw_line in f:
             title = raw_line.strip()
             quantity = f.readline().strip()
             order_list[title] = int(quantity)
-
             ingredient_line = f.readline()
 
             while ingredient_line.strip() != '':
-                record = dict()
-                items = list(map(str.strip, ingredient_line.split('|')))
-                record['ingredient_name'] = items[0]
-                record['quantity'] = int(items[1])
-                record['measure'] = items[2]
-
-                if title in cook_book:
-                    cook_book[title].append(record)
-                else:
-                    cook_book[title] = [record]
+                ingredient_record = create_ingredient_record(ingredient_line)
+                add_record_to_cookbook(title, ingredient_record)
 
                 ingredient_line = f.readline()
+
+
+def create_ingredient_record(ingredient_line):
+    record = dict()
+    items = list(map(str.strip, ingredient_line.split('|')))
+    record['ingredient_name'] = items[0]
+    record['quantity'] = int(items[1])
+    record['measure'] = items[2]
+    return record
+
+
+def add_record_to_cookbook(title, ingredient_record):
+    if title in cook_book:
+        cook_book[title].append(ingredient_record)
+    else:
+        cook_book[title] = [ingredient_record]
+
 
 def get_shop_list_by_dishes():
     shop_list = {}
@@ -40,14 +49,17 @@ def get_shop_list_by_dishes():
                     new_shop_list_item['quantity']
     return shop_list
 
+
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
         print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
                             shop_list_item['measure']))
 
+
 def create_shop_list():
     read_order_list()
     shop_list = get_shop_list_by_dishes()
     print_shop_list(shop_list)
+
 
 create_shop_list()
