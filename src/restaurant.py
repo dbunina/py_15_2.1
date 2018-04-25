189dc4554f357429ede23741feb749c3717a3ba9
Diana Bunina <d.bunina@hh.ru>
2018-04-25 19:16:14 +0300

    fix after review

--- src/restaurant.py
+++ src/restaurant.py
@@ -1,23 +1,19 @@
-# Поскольку количество человек уже указано в исходных данных, код немного переделан
-#
-
-cook_book = dict()
-order_list = dict()
 
 
-def read_order_list():
+def load_cookbook():
+    cook_book = dict()
     with open('order.txt', encoding='cp1251') as f:
         for raw_line in f:
-            title = raw_line.strip()
-            quantity = f.readline().strip()
-            order_list[title] = int(quantity)
+            title = raw_line.strip().lower()
+            next(f)  # skip quantity
             ingredient_line = f.readline()
 
-            while ingredient_line.strip() != '':
+            while ingredient_line.strip():
                 ingredient_record = create_ingredient_record(ingredient_line)
-                add_record_to_cookbook(title, ingredient_record)
+                add_record_to_cookbook(cook_book, title, ingredient_record)
 
                 ingredient_line = f.readline()
+    return cook_book
 
 
 def create_ingredient_record(ingredient_line):
@@ -29,19 +25,19 @@ def create_ingredient_record(ingredient_line):
     return record
 
 
-def add_record_to_cookbook(title, ingredient_record):
+def add_record_to_cookbook(cook_book, title, ingredient_record):
     if title in cook_book:
         cook_book[title].append(ingredient_record)
     else:
         cook_book[title] = [ingredient_record]
 
 
-def get_shop_list_by_dishes():
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
     shop_list = {}
-    for dish in order_list.keys():
-        for ingridient in cook_book[dish]:
-            new_shop_list_item = dict(ingridient)
-            new_shop_list_item['quantity'] *= order_list[dish]
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            new_shop_list_item = dict(ingredient)
+            new_shop_list_item['quantity'] *= person_count
             if new_shop_list_item['ingredient_name'] not in shop_list:
                 shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
             else:
@@ -52,14 +48,17 @@ def get_shop_list_by_dishes():
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
-                            shop_list_item['measure']))
+        print('{} {} {}'.format(
+            shop_list_item['ingredient_name'], shop_list_item['quantity'], shop_list_item['measure'])
+        )
 
 
 def create_shop_list():
-    read_order_list()
-    shop_list = get_shop_list_by_dishes()
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+        .lower().split(', ')
+    cook_book = load_cookbook()
+    shop_list = get_shop_list_by_dishes(cook_book, dishes, person_count)
     print_shop_list(shop_list)
-
 
 create_shop_list()
